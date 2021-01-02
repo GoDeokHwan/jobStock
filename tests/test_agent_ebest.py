a03142d82d19b057371ab9fdf476f4e99ddcb715
DESKTOP-U0S2FK9\DEOKHWAN <dhdh9224@gmail.com>
2021-01-02 14:07:10 +0900

    매도 매수 스케쥴러 완성

--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -3,14 +3,15 @@ from jobstock.agent.ebest import EBest
 import inspect
 import time
 
+
 class TestEBest(unittest.TestCase):
-    def setUp(self):        
+    def setUp(self):
         self.ebest = EBest("DEMO")
         self.ebest.login()
-        
-    def tearDown(self):        
+
+    def tearDown(self):
         self.ebest.logout()
-               
+
     # def test_get_code_list(self):
     #     print(inspect.stack()[0][3])
     #     all_result = self.ebest.get_code_list("ALL")
@@ -25,63 +26,75 @@ class TestEBest(unittest.TestCase):
     #         error_result = None
     #     assert error_result is None
     #     print("result: ", len(all_result), len(kosdaq_result), len(kospi_result))
-        
+
     # def test_get_sotck_price_list_by_code(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_stock_price_by_code("005930", "2")
     #     assert result is not None
     #     print(result)
-        
+
     # def test_get_credit_trend_by_code(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_credit_trend_by_code("005930", "20201221")
     #     assert result is not None
     #     print(result)
-        
+
     # def test_get_short_trend_by_code(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_short_trend_by_code("005930", sdate="20201201", edate="20201221")
     #     assert result is not None
     #     print(result)
-        
+
     # def test_get_agent_trend_by_code(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_agent_trend_by_code("005930", fromdt="20201201", todt="20201221")
     #     assert result is not None
     #     print(result)
-        
+
     # def test_get_account_info(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_account_info()
     #     assert result is not None
     #     print(result)
-        
+
     # def test_order_stock(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.order_stock("005930", "2", "80000", "2", "00")
     #     assert result
     #     print(result)
-        
+
     # def test_order_cancel(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.order_cancel("29515", "A005930", "2")
     #     assert result
     #     print(result)
-        
+
     # def test_order_check(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.order_check("005930",order_no="29515")
     #     assert result
     #     print(result)
-    
+
     # def test_get_current_call_price_by_code(self):
     #     print(inspect.stack()[0][3])
     #     result = self.ebest.get_current_call_price_by_code("005930")
     #     assert result
     #     print(result)
-    
-    def test_get_tick_size(self):
+
+    # def test_get_tick_size(self):
+    #     print(inspect.stack()[0][3])
+    #     result = self.ebest.get_tick_size(500000)
+    #     assert result
+    #     print(result)
+
+    def test_get_price_n_min_by_code(self):
         print(inspect.stack()[0][3])
-        result = self.ebest.get_tick_size(500000)
+        result = self.ebest.get_price_n_min_by_code("20201229", "180640")
+        assert result
+        print(result)
+
+    def test_get_price_n_min_by_code_tick(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_price_n_min_by_code("20201229", "005930", 0)
         assert result
         print(result)
